99cafbc33d2b50aab70d951544544699586aaabe
YurrialSG <yuriinternacional86@gmail.com>
2023-04-04 23:45:58 -0300

    feat: initial commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from "react";
+import { WebView } from "react-native-webview";
+import { View } from "react-native";
 
 export default function App() {
+  const [showWebView, setShowWebView] = useState(false);
+  const popupRef = useRef(null);
+  const uri = "https://app.awari.com.br";
+
+  const jsCode = `
+		document.getElementsByClassName('sc-cRkqwB')[0].style.display = 'none';
+    document.body.style.overflowX = 'hidden';
+  `;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, paddingTop: "10%" }}>
+      <WebView
+        ref={popupRef}
+        source={{ uri: uri }}
+        style={showWebView ? { flex: 1 } : { flex: 0 }}
+        javaScriptEnabled
+        injectedJavaScript={jsCode}
+        startInLoadingState={true}
+        scalesPageToFit={false}
+        onLoadStart={() => {
+          setShowWebView(false);
+        }}
+        onLoadEnd={() => {
+          if (!showWebView) {
+            setShowWebView(true);
+          }
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
